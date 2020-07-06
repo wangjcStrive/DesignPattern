3f17a5eda8535f66d720ad4076f081bb8d98ae8a
jicwang <wangjc857@gmail.com>
2020-07-06 10:19:53 +0800

    Add project files.

---
+++ DesignPattern/CreationalPattern/FactoryMethodPattern.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.CreationalPattern
+{
+    class FactoryMethodPattern
+    {
+    }
+}
---
+++ DesignPattern/Program.cs
@@ -0,0 +1,32 @@
+﻿#define SimpleFactory
+
+using System;
+using DesignPattern.CreationalPattern;
+
+
+namespace DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+#if SimpleFactory
+                Product product;
+                product = SimpleFactory.getProduct("A");
+                product.methodDiff();
+                product = SimpleFactory.getProduct("C");
+                product.methodDiff();
+#endif
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+}
